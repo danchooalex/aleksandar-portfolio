75b8493c7444df295d4ffce6d3d8d7ccb5b30ba7
danchooalex <danchooalexandar@gmail.com>
2024-11-13 10:34:22 +0100

    working the table

--- src/components/homePageComponents/RightSide.jsx
+++ src/components/homePageComponents/RightSide.jsx
@@ -18,9 +18,42 @@ function RightSide() {
         </thead>
         <tbody>
           <tr>
-            <td className={styles.text}>
-              <div className={styles.imageContainer}>
-                <img className={styles.image} src="./coder.JPG" />
+            <td>
+              <div className={styles.text}>
+                <aside className="mb-6 ml-4 flex flex-row gap-3">
+                  <p className="text-sky-300">const</p>
+                  <p className="text-sky-500">coder</p>
+                  <p className="text-purple-500">=</p>
+                  <p className="text-purple-500">{`{`}</p>
+                </aside>
+                <div className="ml-8 mt-2 flex flex-row">
+                  <p className="ml-3 gap-3 text-stone-100">name:</p>
+                  <p className="ml-3">"Aleksandar Dancu",</p>
+                </div>
+                <div className="ml-8 mt-2 flex flex-row">
+                  <p className="ml-3 gap-3 text-stone-100">gender:</p>
+                  <p className="ml-3">"Male",</p>
+                </div>
+                <div className="ml-8 mt-2 flex flex-row">
+                  <p className="ml-3 gap-3 text-stone-100">age:</p>
+                  <p className="ml-3 text-purple-500">41,</p>
+                </div>
+                <div className="ml-8 mt-2 flex flex-row">
+                  <p className="ml-3 gap-3 text-stone-100">company:</p>
+                  <p className="ml-3 text-sky-500">null,</p>
+                </div>
+                <div className="ml-8 mt-2 flex flex-row">
+                  <p className="ml-3 gap-3 text-stone-100">location:</p>
+                  <p className="ml-3">"Belgrade, Serbia",</p>
+                </div>
+                <div className="ml-8 mt-2 flex flex-row">
+                  <p className="ml-3 gap-3 text-stone-100">followers:</p>
+                  <p className="ml-3 text-purple-500">29,</p>
+                </div>
+                <div className="ml-8 mt-2 flex flex-row">
+                  <p className="ml-3 gap-3 text-stone-100">following:</p>
+                  <p className="ml-3 text-purple-500">12,</p>
+                </div>
               </div>
             </td>
           </tr>
