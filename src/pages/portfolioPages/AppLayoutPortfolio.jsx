3f9f038d69a547891c8c4127945bfcaae9f96e3c
danchooalex <danchooalexandar@gmail.com>
2024-11-15 13:49:21 +0100

    homePageMobileDone

--- src/pages/portfolioPages/AppLayoutPortfolio.jsx
+++ src/pages/portfolioPages/AppLayoutPortfolio.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import LeftSide from "../../components/homePageComponents/LeftSide";
 import RightSide from "../../components/homePageComponents/RightSide";
 import styles from "./AppLayoutPortfolio.module.css";
+import Footer from "../../components/homePageComponents/Footer";
 
 function AppLayoutPortfolio() {
   return (
     <main className={styles.container}>
       <aside className={styles.left}>
-        <h3 className={styles.h3L}>{`<React developer/>`}</h3>
+        <h3 className={styles.h3L}>{`<ReactDeveloper/>`}</h3>
         <h1 className={styles.h1L}>Hello, I'm</h1>
         <h2 className={styles.h2L}>Aleksandar Dancu</h2>
       </aside>
@@ -15,6 +16,7 @@ function AppLayoutPortfolio() {
         <LeftSide />
         <RightSide />
       </aside>
+      <Footer />
     </main>
   );
 }
