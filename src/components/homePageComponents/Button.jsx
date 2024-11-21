42dea4b904bc1f60141d4f0df119b876e80ef992
danchooalex <danchooalexandar@gmail.com>
2024-11-21 11:35:31 +0100

    Resume

--- src/components/homePageComponents/Button.jsx
+++ src/components/homePageComponents/Button.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import styles from "./Button.module.css";
+import resume from "../../../public/Resume.pdf";
 
 function Button() {
   return (
-    <button className={styles.btn} onClick={() => alert("NOT ready FREDDY")}>
-      download CV
+    <button className={styles.btn}>
+      <a href={resume} download="Aleksandar Dancu CV">
+        Download CV
+      </a>
     </button>
   );
 }
