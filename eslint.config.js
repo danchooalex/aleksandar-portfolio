5ffa7d0bd3f299b767f8dc35a7464d91f17ab706
danchooalex <danchooalexandar@gmail.com>
2024-11-23 12:29:24 +0100

    v1

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
-
+import tailwind from "eslint-plugin-tailwindcss";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  { files: ["**/*.{js,mjs,cjs,jsx}"] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginReact.configs.flat.recommended,
 ];
